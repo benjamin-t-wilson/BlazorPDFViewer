c9bc68e0122876d5475a03e1d558c0c395c2a050
Benjamin Wilson <whatsup@btwicode.com>
2021-03-19 12:10:11 -0700

    fix print function to be reusable

--- BlazorPDFViewer/wwwroot/js/pdfViewer.js
+++ BlazorPDFViewer/wwwroot/js/pdfViewer.js
@@ -127,6 +127,7 @@ function printPdf(url) {
   var iframe = this._printIframe;
   if (!this._printIframe) {
     iframe = this._printIframe = document.createElement("iframe");
+    iframe.src = url;
     document.body.appendChild(iframe);
 
     iframe.style.display = "none";
@@ -136,7 +137,7 @@ function printPdf(url) {
         iframe.contentWindow.print();
       }, 1);
     };
+  } else {
+    iframe.contentWindow.print();
   }
-
-  iframe.src = url;
 }
